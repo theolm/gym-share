7f938daabc0341e0e96b0855ec8eb7f5479f97a8
Theo <theolm.mota@gmail.com>
2022-12-25 20:10:33 -0300

    Initial commit

---
+++ app/src/main/java/com/theolm/gymShare/presentation/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.theolm.gymShare.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(Modifier.fillMaxSize().background(Color.White)) {
+
+            }
+        }
+    }
+}
